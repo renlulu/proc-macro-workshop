fb97cbaf463f530aca9e33408d2524152edc5b8e
xiaohuo <xiaohuo200@gmail.com>
2020-12-13 10:51:24 +0800

    feat(builder): test02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,35 @@
 use proc_macro::TokenStream;
 use quote::{quote};
+use syn::{parse_macro_input, DeriveInput};
+
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
 
-    let expanded = quote! {};
+    // identifier name
+    let name = input.ident;
+    let expanded = quote! {
+        // the generated impl
+        pub struct CommandBuilder {
+             executable: Option<String>,
+             args: Option<Vec<String>>,
+             env: Option<Vec<String>>,
+             current_dir: Option<String>,
+        }
+        impl #name {
+            fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+
+    };
 
     proc_macro::TokenStream::from(expanded)
 }
