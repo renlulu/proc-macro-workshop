56991e3bf285d5321f74d3af0b5494d0ac051900
xiaohuo <xiaohuo200@gmail.com>
2020-12-13 10:15:33 +0800

    feat(builder): test01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,11 @@
 use proc_macro::TokenStream;
+use quote::{quote};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
 
-    unimplemented!()
+    let expanded = quote! {};
+
+    proc_macro::TokenStream::from(expanded)
 }
