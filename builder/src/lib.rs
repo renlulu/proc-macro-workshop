7920ca80b4b71ed9ca8025ce775d3388caffb27b
xiaohuo <xiaohuo200@gmail.com>
2020-12-13 12:47:46 +0800

    chore: revert test2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -10,47 +10,9 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     // identifier name
     let name = input.ident;
+
     let expanded = quote! {
-        // the generated impl
-        pub struct CommandBuilder {
-             executable: Option<String>,
-             args: Option<Vec<String>>,
-             env: Option<Vec<String>>,
-             current_dir: Option<String>,
-        }
 
-        impl #name {
-            fn builder() -> CommandBuilder {
-                CommandBuilder {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
-                }
-            }
-        }
-
-        impl CommandBuilder {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
-        }
     };
 
     proc_macro::TokenStream::from(expanded)
